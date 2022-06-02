d43efa924727647e3859da529314a2fff5e573f6
joy3333333 <wainaina.joy20@students.dkut.ac.ke>
2022-06-02 10:20:47 -0700

    Initial commit

---
+++ js/animatejscx.js
@@ -0,0 +1,21 @@
+// https://javascript.info/js-animation
+import 'animate.css';
+
+function animate (options) {
+  var start = performance.now();
+
+  requestAnimationFrame(function animate (time) {
+    // timeFraction от 0 до 1
+    var timeFraction = (time - start) / options.duration;
+    if (timeFraction > 1) timeFraction = 1;
+
+    // текущее состояние анимации
+    var progress = options.timing(timeFraction);
+
+    options.draw(progress);
+
+    if (timeFraction < 1) {
+      requestAnimationFrame(animate);
+    }
+  });
+}
