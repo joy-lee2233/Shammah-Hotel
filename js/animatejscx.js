ccff4de1b1f5c616cb27f634f55a5fb4f0d4ffc3
Exclude <shammahnjomo12@gmail.com>
2022-07-06 15:25:03 +0300

    Summary

--- js/animatejscx.js
+++ js/animatejscx.js
@@ -5,11 +5,11 @@ function animate (options) {
   var start = performance.now();
 
   requestAnimationFrame(function animate (time) {
-    // timeFraction от 0 до 1
+  
     var timeFraction = (time - start) / options.duration;
     if (timeFraction > 1) timeFraction = 1;
 
-    // текущее состояние анимации
+    
     var progress = options.timing(timeFraction);
 
     options.draw(progress);
